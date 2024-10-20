00bc6e9ad58463d92d6ffcec8e385d950cd97b90
romeucajamba <romeucajamba@gmail.com>
2024-10-20 13:21:27 +0100

    chore:configurar o type para aceitacao da palavra reservada className

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function Home(){
     return (
         <View>
-            <Text>Home Screen</Text>
+            <Text className="">Home Screen</Text>
         </View>
     )
 }
